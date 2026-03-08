13456db7c921272ce7656ae16398c792c08570fa
René Scholz-Kallies <rene@fantastici.de>
2026-03-08 17:40:33 +0100

    Add public vpn_plugin.h header and VpnPluginRegisterWithRegistrar export.

--- plugins/vpn_plugin/windows/vpn_plugin_c_api.cpp
+++ plugins/vpn_plugin/windows/vpn_plugin_c_api.cpp
@@ -1,12 +1,22 @@
 #include "include/vpn_plugin/vpn_plugin_c_api.h"
+#include "include/vpn_plugin/vpn_plugin.h"
 
 #include <flutter/plugin_registrar_windows.h>
 
 #include "vpn_plugin.h"
 
-void VpnPluginCApiRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
+static void RegisterImpl(FlutterDesktopPluginRegistrarRef registrar) {
   vpn_plugin::VpnPlugin::RegisterWithRegistrar(
       flutter::PluginRegistrarManager::GetInstance()
           ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+}
+
+void VpnPluginCApiRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {
+  RegisterImpl(registrar);
+}
+
+void VpnPluginRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {
+  RegisterImpl(registrar);
 }
